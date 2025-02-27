e3b3fe5648e87f9767c5b817fe7f0a92d00c17ca
Purnendu Dutta <purnendudutta521@gmail.com>
2025-02-27 11:59:02 +0530

    27/02/2025

--- Java String Programs/Main.java
+++ Java String Programs/Main.java
@@ -1,11 +1,46 @@
-import java.util.Scanner;
+//import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Enter a String");
-        Scanner string = new Scanner(System.in);
-        System.out.println(string);
-        string.close();
+//        String name = "Purnendu";
+//        System.out.println(name); //prints the string
+//        
+//        int value = name.length();
+//        System.out.println(value); //how many alphabets in the string
+//
+//        String lString = name.toLowerCase();
+//        System.out.println(lString); //makes the string to lowercase
+//
+//        String uString = name.toUpperCase();
+//        System.out.println(uString); //makes the string to uppercase
+//
+//        String nonTrimmedString = "      PurnenduDutta       ";
+//        System.out.println(nonTrimmedString); //this will not trim the spaces.
+//        System.out.println(nonTrimmedString.trim()); //when .trim is used , this will trim the spaces.
+//
+//        System.out.println(name.substring(1));
+//        System.out.println(name.substring(1,5));
+//
+//        System.out.println(name.replace('r', 'e'));
+//        System.out.println(name.replace("r", "rrr"));
+//
+//        System.out.println(name.startsWith("Pur"));
+//        System.out.println(name.endsWith("Pur"));
+//
+//        System.out.println(name.charAt(1));
+//
+//        String modifiedName = "Purrnerrndu";
+//        System.out.println(modifiedName.indexOf("rr"));
+//        System.out.println(modifiedName.indexOf("rr",3));
+//        System.out.println(modifiedName.lastIndexOf("rr",3));
+//
+//        System.out.println(name.equals("Purnendu"));
+//        System.out.println(name.equalsIgnoreCase("purnendu"));
+//
+//        System.out.println("I am escape sequence \\ double quote"); //inserts single backslash
+//        System.out.println("I am escape sequence \n double quote"); //inserts new line
+//        System.out.println("I am escape sequence \t double quote"); // inserts tab
+//        System.out.println("I am escape sequence \b double quote"); //inserts backspace
 
     }
 }
