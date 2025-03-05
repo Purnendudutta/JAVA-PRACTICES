5e894e5cb44d5156efafab4063e9cd66bf0d229d
Purnendu Dutta <purnendudutta521@gmail.com>
2025-03-05 10:08:49 +0530

    05/03/2025

--- practice.java/Methods.java
+++ practice.java/Methods.java
@@ -1,31 +1,21 @@
 public class Methods {
-//    public static void main(String[] args) {
-//        int a = 5;
-//        int b = 7;
-//        int c;
-//        if(a>b) {
-//            c = a+b;
-//            System.out.println(c);
-//        }
-//        else {
-//            c = (a+b)*5;
-//            System.out.println(c);
-//        }
-//    }
-
+    
     static int logic(int x, int y) {
-        x = 5;
-        y = 7;
         int z;
         if(x>y) {
             z = x+y;
-            System.out.println(z);
         }
         else {
             z = (x+y)*5;
-            System.out.println(z);
         }
-        System.out.println(z);
         return z;
+    }
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+        int c;
+        c = logic(a,b);
+        System.out.println(c);
     }
 }
